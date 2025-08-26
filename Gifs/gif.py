a4f9c84e78f4640482a9d6693cb0b2e217ca1aba
Jackweather <159730073+Jackweather@users.noreply.github.com>
2025-08-25 20:13:10 -0400

    Change output GIF path to '/var/data'

--- Gifs/gif.py
+++ Gifs/gif.py
@@ -5,7 +5,7 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def create_gif_from_folder(src_folder):
     folder_name = os.path.basename(src_folder)
-    dst_gif = os.path.join(os.path.dirname(__file__), f'{folder_name}.gif')
+    dst_gif = os.path.join('/var/data', f'{folder_name}.gif')
     png_files = sorted(
         [f for f in os.listdir(src_folder) if f.endswith('.png')]
     )
@@ -21,7 +21,7 @@ def create_gif_from_folder(src_folder):
     else:
         print(f"No PNG images found in source folder: {src_folder}")
 
-base_static = os.path.join(os.path.dirname(__file__), '..', 'gfsmodel', 'GFS', 'static')
+base_static = os.path.join('/var/data', 'gfsmodel', 'GFS', 'static')
 
 folders = [
     'PRATEGFS',
