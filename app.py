76d868f6feaa4a13bd2e5bc07520e3385bd3abaa
Jackweather <159730073+Jackweather@users.noreply.github.com>
2025-09-12 21:25:12 -0400

    Update print statement to show 'Goodbye World'

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ def serve_totalsnowfall_15to1_image(filename):
         abort(404)
     return send_from_directory(directory, filename)
 
-@app.route('/GFS/static/THICKNESS/<path:filename>')
+@app.route('/GFS/static/thickness/<path:filename>')
 def serve_thickness_image(filename):
     directory = os.path.join(BASE_DATA_DIR, 'GFS', 'static', 'THICKNESS')
     abs_path = os.path.join(directory, filename)
